7a327eabbbd32078cfd834bf8380dc5d23d49c6d
Shems <104856722+shems95v@users.noreply.github.com>
2025-05-05 15:19:04 +0200

    Update affichage.c

--- affichage.c
+++ affichage.c
@@ -3,35 +3,6 @@
 #include <string.h>
 #include "affichage.h"
 
-void afficher_carte_colorée(Carte carte) {
-    char *couleurTexte, *couleurFond;
-
-    if (!carte.visible) {
-        couleurTexte = BLANC;
-        couleurFond = BG_NOIR;
-    } else {
-        switch (carte.valeur) {
-            case -2: couleurTexte = NOIR; couleurFond = BG_MAGENTA; break;
-            case -1: couleurTexte = NOIR; couleurFond = BG_CYAN; break;
-            case 0:  couleurTexte = NOIR; couleurFond = BG_BLEU; break;
-            case 1:  couleurTexte = NOIR; couleurFond = BG_VERT; break;
-            case 2:  couleurTexte = NOIR; couleurFond = BG_JAUNE; break;
-            case 3:  couleurTexte = NOIR; couleurFond = BG_ROUGE; break;
-            case 4:  couleurTexte = BLANC; couleurFond = BG_MAGENTA; break;
-            case 5:  couleurTexte = NOIR; couleurFond = BG_BLANC; break;
-            case 6:  couleurTexte = BLANC; couleurFond = BG_CYAN; break;
-            case 7:  couleurTexte = NOIR; couleurFond = BG_VERT; break;
-            case 8:  couleurTexte = NOIR; couleurFond = BG_JAUNE; break;
-            case 9:  couleurTexte = BLANC; couleurFond = BG_BLEU; break;
-            case 10: couleurTexte = NOIR; couleurFond = BG_ROUGE; break;
-            case 11: couleurTexte = NOIR; couleurFond = BG_BLANC; break;
-            case 12: couleurTexte = BLANC; couleurFond = BG_ROUGE; break;
-            default: couleurTexte = BLANC; couleurFond = BG_NOIR; break;
-        }
-    }
-
-    printf("%s%s│   %2d    │%s\n", couleurFond, couleurTexte, carte.valeur, RESET);
-}
 
 void afficher_scores_tries(Joueur joueurs[], int nbJoueurs) {
     for (int i = 0; i < nbJoueurs; i++) {
