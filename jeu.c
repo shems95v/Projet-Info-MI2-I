f4b73327a49ff53646636653cfbfd99678f79263
Shems <104856722+shems95v@users.noreply.github.com>
2025-05-11 15:17:00 +0200

    Update jeu.c

--- jeu.c
+++ jeu.c
@@ -20,28 +20,66 @@ void afficher_carte_ligne(Carte carte, int ligne) {
         // Définition des couleurs selon la valeur de la carte
         char *couleurTexte, *couleurFond;
         switch (carte.valeur) {
-            case -2:  couleurTexte = BLANC; couleurFond = "\x1b[48;5;52m";  break;
-            case -1:  couleurTexte = BLANC; couleurFond = "\x1b[48;5;130m"; break;
-            case 0:   couleurTexte = NOIR;  couleurFond = "\x1b[48;5;226m"; break;
-            case 1:   couleurTexte = NOIR;  couleurFond = "\x1b[48;5;118m"; break;
-            case 2:   couleurTexte = NOIR;  couleurFond = "\x1b[48;5;33m";  break;
-            case 3:   couleurTexte = NOIR;  couleurFond = "\x1b[48;5;208m"; break;
-            case 4:   couleurTexte = BLANC; couleurFond = "\x1b[48;5;129m"; break;
-            case 5:   couleurTexte = NOIR;  couleurFond = "\x1b[48;5;181m"; break;
-            case 6:   couleurTexte = NOIR;  couleurFond = "\x1b[48;5;44m";  break;
-            case 7:   couleurTexte = BLANC; couleurFond = "\x1b[48;5;160m"; break;
-            case 8:   couleurTexte = NOIR;  couleurFond = "\x1b[48;5;123m"; break;
-            case 9:   couleurTexte = NOIR;  couleurFond = "\x1b[48;5;210m"; break;
-            case 10:  couleurTexte = BLANC; couleurFond = "\x1b[48;5;25m";  break;
-            case 11:  couleurTexte = BLANC; couleurFond = "\x1b[48;5;34m";  break;
-            case 12:  couleurTexte = NOIR;  couleurFond = "\x1b[48;5;220m"; break;
-            default:  couleurTexte = BLANC; couleurFond = BG_NOIR;          break;
+            case -20: couleurTexte = BLANC; couleurFond = "\x1b[48;5;17m"; break;
+            case -19: couleurTexte = BLANC; couleurFond = "\x1b[48;5;18m"; break;
+            case -18: couleurTexte = BLANC; couleurFond = "\x1b[48;5;19m"; break;
+            case -17: couleurTexte = BLANC; couleurFond = "\x1b[48;5;20m"; break;
+            case -16: couleurTexte = BLANC; couleurFond = "\x1b[48;5;21m"; break;
+            case -15: couleurTexte = BLANC; couleurFond = "\x1b[48;5;22m"; break;
+            case -14: couleurTexte = BLANC; couleurFond = "\x1b[48;5;23m"; break;
+            case -13: couleurTexte = BLANC; couleurFond = "\x1b[48;5;24m"; break;
+            case -12: couleurTexte = BLANC; couleurFond = "\x1b[48;5;25m"; break;
+            case -11: couleurTexte = BLANC; couleurFond = "\x1b[48;5;26m"; break;
+            case -10: couleurTexte = BLANC; couleurFond = "\x1b[48;5;27m"; break;
+            case -9:  couleurTexte = BLANC; couleurFond = "\x1b[48;5;28m"; break;
+            case -8:  couleurTexte = BLANC; couleurFond = "\x1b[48;5;29m"; break;
+            case -7:  couleurTexte = BLANC; couleurFond = "\x1b[48;5;30m"; break;
+            case -6:  couleurTexte = BLANC; couleurFond = "\x1b[48;5;31m"; break;
+            case -5:  couleurTexte = BLANC; couleurFond = "\x1b[48;5;32m"; break;
+            case -4:  couleurTexte = BLANC; couleurFond = "\x1b[48;5;33m"; break;
+            case -3:  couleurTexte = BLANC; couleurFond = "\x1b[48;5;34m"; break;
+            case -2:  couleurTexte = BLANC; couleurFond = "\x1b[48;5;35m"; break;
+            case -1:  couleurTexte = BLANC; couleurFond = "\x1b[48;5;36m"; break;
+            case 0:   couleurTexte = BLANC; couleurFond = "\x1b[48;5;37m"; break;
+            case 1:   couleurTexte = NOIR;  couleurFond = "\x1b[48;5;38m"; break;
+            case 2:   couleurTexte = NOIR;  couleurFond = "\x1b[48;5;39m"; break;
+            case 3:   couleurTexte = NOIR;  couleurFond = "\x1b[48;5;40m"; break;
+            case 4:   couleurTexte = NOIR;  couleurFond = "\x1b[48;5;41m"; break;
+            case 5:   couleurTexte = NOIR;  couleurFond = "\x1b[48;5;42m"; break;
+            case 6:   couleurTexte = NOIR;  couleurFond = "\x1b[48;5;43m"; break;
+            case 7:   couleurTexte = NOIR;  couleurFond = "\x1b[48;5;44m"; break;
+            case 8:   couleurTexte = NOIR;  couleurFond = "\x1b[48;5;45m"; break;
+            case 9:   couleurTexte = NOIR;  couleurFond = "\x1b[48;5;46m"; break;
+            case 10:  couleurTexte = NOIR;  couleurFond = "\x1b[48;5;47m"; break;
+            case 11:  couleurTexte = NOIR;  couleurFond = "\x1b[48;5;48m"; break;
+            case 12:  couleurTexte = NOIR;  couleurFond = "\x1b[48;5;49m"; break;
+            case 13:  couleurTexte = NOIR;  couleurFond = "\x1b[48;5;50m"; break;
+            case 14:  couleurTexte = NOIR;  couleurFond = "\x1b[48;5;51m"; break;
+            case 15:  couleurTexte = BLANC;  couleurFond = "\x1b[48;5;52m"; break;
+            case 16:  couleurTexte = NOIR;  couleurFond = "\x1b[48;5;53m"; break;
+            case 17:  couleurTexte = NOIR;  couleurFond = "\x1b[48;5;54m"; break;
+            case 18:  couleurTexte = NOIR;  couleurFond = "\x1b[48;5;55m"; break;
+            case 19:  couleurTexte = NOIR;  couleurFond = "\x1b[48;5;56m"; break;
+            case 20:  couleurTexte = NOIR;  couleurFond = "\x1b[48;5;57m"; break;
+            default:  couleurTexte = BLANC; couleurFond = BG_NOIR; break;
         }
 
         // Affichage ligne par ligne de la carte visible
         switch (ligne) {
             case 0: printf("╭────────────╮ "); break; // bord supérieur
-            case 1: printf("│%s%s    %2d     %s │ ", couleurFond, couleurTexte, carte.valeur, RESET); break; // valeur centrée
+            case 1: {
+                char buffer[8];
+                snprintf(buffer, sizeof(buffer), "%d", carte.valeur);
+                int totalWidth = 12;
+                int len = strlen(buffer);
+                int pad = (totalWidth - len) / 2;
+                printf("│%s%s", couleurFond, couleurTexte);
+                for (int i = 0; i < pad; i++) printf(" ");
+                printf("%s", buffer);
+                for (int i = 0; i < totalWidth - len - pad; i++) printf(" ");
+                printf("%s│ ", RESET);
+                break;
+            }
             case 2: printf("╰────────────╯ "); break; // bord inférieur
         }
     } else {
